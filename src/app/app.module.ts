5a8e1990a4a4f140b480abd5140d038a3d2e770b
sagi <contact@sagishutrit.com>
2025-10-24 15:18:41 +0200

    Add APP_INITIALIZER for config loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
-import {  NgModule } from '@angular/core';
+import {  NgModule,  APP_INITIALIZER  } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { DateService } from './store/date.service';
+import { MessageService } from './store/message.service';
 import { ContactFormComponent } from './contact-form/contact-form.component';
 import { MapboxComponent } from './mapbox/mapbox.component';
 import { CommonModule } from '@angular/common';
+import { ConfigService } from './config.service';
+
+export function initConfig(config: ConfigService) {
+  return () => config.loadConfig();
+}
 
 @NgModule({
 
@@ -27,7 +32,14 @@ import { CommonModule } from '@angular/common';
     MapboxComponent
   ],
   exports:[],
-  providers: [DateService],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initConfig,
+      deps: [ConfigService],
+      multi: true,
+    },
+    MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
