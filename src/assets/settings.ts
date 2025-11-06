a2a85884ce74909b3e9d07b60b5c2f190afcc1b4
sagi <contact@sagishutrit.com>
2025-11-06 23:24:19 +0100

    Update apiToken in AppSettings

--- src/assets/settings.ts
+++ src/assets/settings.ts
@@ -2,8 +2,8 @@ export const AppSettings = {
     apiUrl: "",
     host:"zommer",
     map:{
-        apiToken:"",
-        imgPath: "https://zommers.nl/images/Logo20.png",
+        apiToken:"SDTEST422S",
+        imgPath: "https://zommers.nl/images/Logo20.png",    
         defaultZoom: 9,
         defaultCenter: [4.30809, 52.07185]
     }
